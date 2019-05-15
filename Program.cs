2ede2e1e1930486e430f7cca3eb10ffdcc03f504
mvernon27 <mvernon2705@gmail.com>
2019-05-15 13:48:09 -0400

    Version 2.0 Working

--- Program.cs
+++ Program.cs
@@ -10,77 +10,100 @@ namespace FortuneTeller
 
             Console.WriteLine("What is your first name?");
             string fName = Console.ReadLine();
+
             Console.WriteLine("What is your last name?");
             string lName = Console.ReadLine();
+
             Console.WriteLine("How old are you?");
             int age = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("What is the numerical value of your birth-month?");
             int bMonth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is your favorite ROYGBIV color? Type 'help' for color list");
 
+            Console.WriteLine("What is your favorite ROYGBIV color? Type 'help' for color list or 'Enter' key to continue.");
+
+
+            
             if (Console.ReadLine() == "help")
             {
-                Console.WriteLine("R=Red, O=Orange, Y=Yellow, G=Green, B=Blue, I=Indigo, V=Violet: Select one to continue.");
+                Console.WriteLine("R=Red, O=Orange, Y=Yellow, G=Green, B=Blue, I=Indigo, V=Violet: Select one to continue.");         //Need to Loop***
             }
 
-            string color = Console.ReadLine();
-            Console.WriteLine("How many sibling do you have?");
-            int siblings = Convert.ToInt32(Console.ReadLine());
 
-            {
-                if (age % 2 == 0)           //even
 
-                {
-                    Console.WriteLine("You will retire in 100 years: Press a key to continue.");
-                }
-                else
-                {
-                    Console.WriteLine("You will retire next year.");              //odd
-                }
+            Console.WriteLine("What is your favorite ROYGBIV color? ");
+                string color = Console.ReadLine();
+            
 
-                if (siblings == 0)
-                {
-                    Console.WriteLine("Your vacation home will be Cleveland, OH.");
-                }
-                else if (siblings == 1)
-                {
-                    Console.WriteLine("Your vacation home will be Detroit, MI");
-                }
-                else if (siblings == 2)
-                {
-                    Console.WriteLine("Your vacation home will be Chicago, IL");
-                }
-                else if (siblings == 3)
-                {
-                    Console.WriteLine("Your vacation home will be Columbus, OH");
-                }
-                else if (siblings > 3)
-                {
-                    Console.WriteLine("Your vacation home will be Venice, Italy");
-                }
-                else if (siblings < 0)
-                {
-                    Console.WriteLine("Your vacation home will be Las Angeles, CA");
-                }
+
+
+                Console.WriteLine("How many sibling do you have?");
+                int siblings = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Your name is " + fName + " " + lName);
+                
+                    if (age % 2 == 0)           //even
+
+                    {
+                        Console.WriteLine("You will retire in 100 years.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You will retire next year.");              //odd
+                    }
+
+                    if (siblings == 0)
+                    {
+                        Console.WriteLine("Your vacation home will be Cleveland, OH.");
+                    }
+                    else if (siblings == 1)
+                    {
+                        Console.WriteLine("Your vacation home will be Detroit, MI");
+                    }
+                    else if (siblings == 2)
+                    {
+                        Console.WriteLine("Your vacation home will be Chicago, IL");
+                    }
+                    else if (siblings == 3)
+                    {
+                        Console.WriteLine("Your vacation home will be Columbus, OH");
+                    }
+                    else if (siblings > 3)
+                    {
+                        Console.WriteLine("Your vacation home will be Venice, Italy");
+                    }
+                    else if (siblings < 0)
+                    {
+                        Console.WriteLine("Your vacation home will be Las Angeles, CA");
+                    }
+                
+
+                if (color == "R") { Console.WriteLine("Your mode of transportation will be bicycle."); }
+                if (color == "O") { Console.WriteLine("Your mode of transportation will be tricycle."); }
+                if (color == "Y") { Console.WriteLine("Your mode of transportation will be car."); }
+                if (color == "G") { Console.WriteLine("Your mode of transportation will be truck."); }
+                if (color == "B") { Console.WriteLine("Your mode of transportation will be rocket."); }
+                if (color == "I") { Console.WriteLine("Your mode of transportation will be walking."); }
+                if (color == "V") { Console.WriteLine("Your mode of transportation will be running."); }
+
+                if (bMonth == 1-4) { Console.WriteLine("Your bank balance will be $1.00"); }
+                if (bMonth == 5-8) { Console.WriteLine("Your bank balance will be $1,000,000,000"); }
+                if (bMonth == 9 - 12) { Console.WriteLine("Your bank balance will be $1,000"); }
+                if (bMonth != 1-12) { Console.WriteLine("Your bank balance will be $0.00"); }
+
+
+
+
+
+
+
+
             }
-            if (color == "R") {Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "O)  {Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "Y") { Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "G") { Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "B") { Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "I") { Console.WriteLine("Your mode of transportation will be."); }
-            if (color == "V") { Console.WriteLine("Your mode of transportation will be."); }
 
-
-
-
-
-
-
-
-        }
-           
         }
     }
+
+    
 
 
