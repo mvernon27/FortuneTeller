404fd35acc0eafc8a9174952c4c9048f95867cb1
mvernon27 <mvernon2705@gmail.com>
2019-05-16 10:30:55 -0400

    Fortune now follows directions

--- Program.cs
+++ Program.cs
@@ -24,16 +24,16 @@ namespace FortuneTeller
 
             Console.WriteLine("What is your favorite ROYGBIV color? Type 'help' for color list or 'Enter' key to continue.");
 
-            if (Console.ReadLine() == "help")
+            if (Console.ReadLine() == "help")                                                                                           //Needs Loop
             {
-                Console.WriteLine("R=Red, O=Orange, Y=Yellow, G=Green, B=Blue, I=Indigo, V=Violet:");         
+                Console.WriteLine("R=Red, O=Orange, Y=Yellow, G=Green, B=Blue, I=Indigo, V=Violet:");
             }
 
-            Console.WriteLine("What is your favorite ROYGBIV color? ");
-                string color = Console.ReadLine();
-         
-                Console.WriteLine("How many sibling do you have?");
-                int siblings = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your favorite ROYGBIV color? ");                         
+            string color = Console.ReadLine();
+
+            Console.WriteLine("How many sibling do you have?");
+            int siblings = Convert.ToInt32(Console.ReadLine());
 
 
             //End Variable declarations
@@ -47,107 +47,108 @@ namespace FortuneTeller
             //Fortune write start**
 
 
-            Console.WriteLine("Your name is " + fName + " " + lName);                     //First & Last name
+            Console.Write(fName + " " + lName + " ");                     //First & Last name
 
 
-            if (age % 2 == 0)         
+            if (age % 2 == 0)
 
-                    {
-                        Console.WriteLine("You will retire in 100 years.");           //even age
-                    }
-                    else
-                    {
-                        Console.WriteLine("You will retire next year.");              //odd age
-                    }
-
-
-
-                if (siblings > 4)                                                                     //Number of siblings
-                    {
-                        Console.WriteLine("Your vacation home will be Cleveland, OH.");                    
-                    }
-                    else if (siblings == 1)
-                    {
-                        Console.WriteLine("Your vacation home will be Detroit, MI");
-                    }
-                    else if (siblings == 2)
-                    {
-                        Console.WriteLine("Your vacation home will be Chicago, IL");
-                    }
-                    else if (siblings == 3)
-                    {
-                        Console.WriteLine("Your vacation home will be Columbus, OH");
-                    }
-                    else if (siblings == 4)
-                    {
-                        Console.WriteLine("Your vacation home will be Venice, Italy");
-                    }
-                    else if (siblings < 0)
-                    {
-                        Console.WriteLine("Your vacation home will be Las Angeles, CA");
-                    }
-                
-             
-
-                if (color == "R")                                                                      //Favorite color
             {
-                Console.WriteLine("Your mode of transportation will be bicycle.");
+                Console.Write("will retire in 100 years with ");           //even age
             }
-                if (color == "O")
+            else
             {
-                Console.WriteLine("Your mode of transportation will be tricycle.");
-            }
-                if (color == "Y")
-            {
-                Console.WriteLine("Your mode of transportation will be car.");
-            }
-                if (color == "G")
-            {
-                Console.WriteLine("Your mode of transportation will be truck.");
-            }
-                if (color == "B")
-            {
-                Console.WriteLine("Your mode of transportation will be rocket.");
-            }
-                if (color == "I")
-            {
-                Console.WriteLine("Your mode of transportation will be walking.");
-            }
-                if (color == "V")
-            {
-                Console.WriteLine("Your mode of transportation will be running.");
+                Console.Write("will retire next year with ");              //odd age
             }
 
 
             if (bMonth >= 1 && bMonth <= 4)                                                                       //Birth Month
             {
-                Console.WriteLine("Your bank balance will be $1.00");
+                Console.Write("$1.00 in the bank, ");
             }
-                if (bMonth >= 5 && bMonth <= 8)
+            if (bMonth >= 5 && bMonth <= 8)
             {
-                Console.WriteLine("Your bank balance will be $1,000,000,000");
+                Console.Write("$1,000,000,000 in the bank, ");
             }
-                if (bMonth >= 9 && bMonth <= 12)
+            if (bMonth >= 9 && bMonth <= 12)
             {
-                Console.WriteLine("Your bank balance will be $1,000");
+                Console.Write("$1,000 in the bank, ");
             }
-                if (bMonth < 0)
+            if (bMonth < 0)
             {
-                Console.WriteLine("Your bank balance will be $0.00");
+                Console.Write("$0.00 in the bank,");
             }
-                if (bMonth > 12)
-                {
+            if (bMonth > 12)
+            {
 
-                    Console.WriteLine("Your bank balance will be $0.00");
-                }
+                Console.Write("$0.00 in the bank, ");
+            }
+
+
+                if (siblings > 4)                                                                     //Number of siblings
+            {
+                Console.Write("a vacation home in Cleveland, OH, ");
+            }
+            else if (siblings == 1)
+            {
+                    Console.Write("a vacation home in Detroit, MI, ");
+            }
+            else if (siblings == 2)
+            {
+                Console.Write("a vacation home in Chicago, IL, ");
+            }
+            else if (siblings == 3)
+            {
+                Console.Write("a vacation home in Columbus, OH, ");
+            }
+            else if (siblings == 4)
+            {
+                Console.Write("a vacation home in Venice, Italy, ");
+            }
+            else if (siblings < 0)
+            {
+                Console.Write("a vacation home in Las Angeles, CA, ");
+            }
+
+
+
+            if (color.ToUpper().Equals("R"))                                                                      //Favorite color
+            {
+                Console.Write("and travel by bicycle.");
+            }
+            if (color.ToUpper().Equals("O"))
+            {
+                Console.Write("and travel by tricycle.");
+            }
+            if (color.ToUpper().Equals("Y"))
+            {
+                Console.Write("and travel by car.");
+            }
+            if (color.ToUpper().Equals("G"))
+            {
+                Console.Write("and travel by truck.");
+            }
+            if (color.ToUpper().Equals("B"))
+            {
+                Console.Write("and travel by rocket.");
+            }
+            if (color.ToUpper().Equals("I"))
+            {
+                Console.Write("and travel by walking.");
+            }
+            if (color.ToUpper().Equals("V"))
+            {
+                Console.Write("and travel by running.");
+            }
+
+
             
-
-
             }
+
+
 
         }
+
     }
 
-    
 
 
